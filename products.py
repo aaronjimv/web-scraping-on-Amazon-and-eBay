c6fb442838723962c42828851788bdb3c40b7a76
Aaron Jimenez <aaronjimv@gmail.com>
2023-10-12 16:55:46 -0800

    se agrego la documentacion al archivo products.py

--- products.py
+++ products.py
@@ -2,6 +2,16 @@ from connection import *
 
 class Products:
     def __init__(self, name, amazon_url, ebay_url, amazon_price, ebay_price):
+        '''
+        Initializes a Products object with the provided attributes.
+
+        Parameters:
+            name (str): The name of the product.
+            amazon_url (str): The URL of the product on Amazon.
+            ebay_url (str): The URL of the product on eBay.
+            amazon_price (float): The price of the product on Amazon.
+            ebay_price (float): The price of the product on eBay.
+        '''
         self.name = name
         self.amazon_url = amazon_url
         self.ebay_url = ebay_url
@@ -9,6 +19,12 @@ class Products:
         self.ebay_price = ebay_price
 
     def save_products(self):
+        '''
+        Saves the product information into the database.
+
+        Returns:
+            str: A message indicating whether the operation was successful or an error occurred.
+        '''
         try:
             conn = connect()
             cursor = conn.cursor()
@@ -22,6 +38,13 @@ class Products:
             return "ERROR"+err
 
     def get_products(self):
+        '''
+        Retrieves all products from the database.
+
+        Returns:
+            list: A list of tuples representing product information (name, amazon_url, ebay_url, amazon_price, ebay_price).
+            If an error occurs, returns an error message.
+        '''
         try:
             conn = connect()
             cursor = conn.cursor() 
