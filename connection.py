b73556acc52ae659848e3d237854ce63528c1ca7
Aaron Jimenez <aaronjimv@gmail.com>
2023-10-12 17:17:45 -0800

    se agrego la documentacion al archivo connection.py

--- connection.py
+++ connection.py
@@ -1,6 +1,13 @@
 import mysql.connector as mysql
 
 def connect():
+    '''
+    Establishes a connection to the MySQL database.
+
+    Returns:
+        mysql.connector.connection_cext.CMySQLConnection: A MySQL connection object if successful.
+        If an error occurs during connection, prints an error message and returns None.
+    '''
     try:
         connection = mysql.connect(
             host = 'localhost',
