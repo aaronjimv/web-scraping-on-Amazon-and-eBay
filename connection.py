bd58cc6b3519dde68ed7622b132715d16f7c3ef4
Aaron Jimenez <aaronjimv@gmail.com>
2023-10-31 09:45:38 -0800

    se aplico el pep8 a connection.py

--- connection.py
+++ connection.py
@@ -1,20 +1,24 @@
 import mysql.connector as mysql
 
+
 def connect():
     '''
     Establishes a connection to the MySQL database.
 
     Returns:
-        mysql.connector.connection_cext.CMySQLConnection: A MySQL connection object if successful.
-        If an error occurs during connection, prints an error message and returns None.
+        mysql.connector.connection_cext.CMySQLConnection:
+        A MySQL connection object if successful.
+
+        If an error occurs during connection, prints an
+        error message and returns None.
     '''
     try:
         connection = mysql.connect(
-            host = 'localhost',
-            port = 33065,
-            user = 'root',
-            password = '',
-            database = 'web_scraping'
+            host='localhost',
+            port=33065,
+            user='root',
+            password='',
+            database='web_scraping'
         )
         print("\n- Connected to the database -")
         return connection
